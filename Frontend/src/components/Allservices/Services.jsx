de3ab24458167a833d821105ea00956f029f6109
AkashDeepSaurabh <akashdeepsaurabh7@gmail.com>
2024-12-09 22:02:54 +0530

    Updated the css for buttons and hover and the background of the pages

--- Frontend/src/components/Allservices/Services.jsx
+++ Frontend/src/components/Allservices/Services.jsx
@@ -13,22 +13,22 @@ const Services = () => {
                 <div className="service-card">
                     <h2>Web Development</h2>
                     <p>We create responsive and user-friendly websites that drive engagement and growth. Our team uses the latest technologies to deliver high-quality web solutions.</p>
-                    <button onClick={() => navigate('/services/web-development')}>Get in Touch</button>
+                    <button onClick={() => navigate('/services/web-development')}>Explore</button>
                 </div>
                 <div className="service-card">
                     <h2>Quality Assurance</h2>
                     <p>Our QA services ensure that your products are bug-free and meet the highest quality standards. We provide comprehensive testing solutions tailored to your needs.</p>
-                    <button onClick={() => navigate('services/quality-assurance')}>Get in Touch</button>
+                    <button onClick={() => navigate('services/quality-assurance')}>Explore</button>
                 </div>
                 <div className="service-card">
                     <h2>Digital Marketing & SEO</h2>
                     <p>Our digital marketing and SEO services are designed to increase your online visibility and drive traffic to your website. Let us help you reach your target audience effectively.</p>
-                    <button onClick={() => navigate('/services/digital-seo')}>Get in Touch</button>
+                    <button onClick={() => navigate('/services/digital-seo')}>Explore</button>
                 </div>
                 <div className="service-card">
                     <h2>Blogs</h2>
                     <p>Explore our insights and expertise through our blog. From tech trends to practical tips, we share knowledge to help you stay informed and make strategic technology decisions.</p>
-                    <button onClick={() => navigate('/blogs/all-categories')}>Explore Blogs</button>
+                    <button onClick={() => navigate('/blogs/all-categories')}>Explore</button>
                 </div>
             </div>
         </div>
